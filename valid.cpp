7b92973a3864b4bc3fafa30ac598f653d8a26041
slscholz <72630237+slscholz@users.noreply.github.com>
2020-10-09 17:15:37 -0400

    Add files via upload

---
+++ valid.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+int main()
+{
+	string word;
+	vector<string> keywords;
+	vector<string> identifiers;
+	
+	getline(cin,word);	
+	while(word.compare("LAST")!=0)
+	{
+		keywords.push_back(word);
+		getline(cin,word);
+	}
+	getline(cin,word);
+	while(word.compare("END")!=0)
+	{
+		if(identifiers.empty())
+		{
+			identifiers.push_back(word);
+		}
+		else
+		{
+			identifiers.insert(identifiers.begin(),word);
+		}
+		getline(cin,word);
+	}
+	while(!identifiers.empty())
+	{
+		cout<<identifiers.back()<<" is ";
+		bool key=true;
+		for(int i=0; i<keywords.size(); i++)
+		{
+			if(identifiers.back().compare(keywords.at(i))==0)
+			{
+				key=false;
+			}
+			else if(!isalpha(identifiers.back().at(0)) && identifiers.back().at(0)-'$' !=0 &&  identifiers.back().at(0)-'_' !=0 )
+			{
+				key=false;
+			}
+			for(i=1; i< identifiers.back().length(); i++)
+			{
+				if(!isalnum(identifiers.back().at(i)) && identifiers.back().at(i)-'$' !=0 && identifiers.back().at(i)-'_' !=0 )
+				{
+					key=false;
+				}
+			}
+		}
+		if(key)
+		{
+			cout<<"valid"<<endl;
+		}
+		else
+		{
+			cout<<"invalid"<<endl;
+		}
+		identifiers.pop_back();
+
+	}
+
+	return 0;
+
+}
---
+++ validator.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+#include <stdlib.h>
+
+int main()
+{
+	int cases;
+	do{
+	
+		cout<<"Enter the number of cases"<<endl;
+		cin>>cases;					//might want to check if it is valid data type
+
+	}while(cases<=0);
+	
+	cin.ignore();
+	//string ISBN;
+	for(int i=0; i<cases; i++)
+	{
+		int total=0;
+		int answer=0;
+		//cin.clear();
+		string ISBN;
+		getline(cin,ISBN);
+		
+		if(ISBN.length()==10)
+		{	
+			cout<<"worky"<<endl;
+			for(int j=0; j<ISBN.length(); j++)
+			{	
+				if(ISBN.at(j)=='X' && j==9)
+				{
+					total+=(j+1)*10;
+				}
+				else if(ISBN.at(j)=='X' && j!=9)
+				{
+					cout<<"false"<<endl;
+				}
+				else
+				{
+					total+=(j+1)* (int) ISBN.at(j);// atoi(ISBN.at(j)+ "");
+				}
+			}
+			answer=total%11;
+			if(answer==0)
+			{
+				cout<<"true"<<endl;
+			}
+			else
+			{	
+				cout<<"false"<<endl;
+			}			
+		}
+		else
+		{
+			cout<<"false"<<endl;
+		}
+	}
+
+	return 0;
+}
